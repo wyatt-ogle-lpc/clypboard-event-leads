b6502c44e2bf33ebe32d614300fbfff4a967ea97
Wyatt Ogle <wogle@Wyatts-MacBook-Pro.local>
2025-06-02 11:46:53 -0700

    removed duplicate migration column

--- db/migrate/20250602180647_add_already_raffled_variable.rb
+++
@@ -1,5 +0,0 @@
-class AddAlreadyRaffledVariable < ActiveRecord::Migration[8.0]
-  def change
-    add_column :entries, :raffled, :boolean
-  end
-end
--- db/migrate/20250602181544_add_already_raffled_variable_retry.rb
+++
@@ -1,4 +0,0 @@
-class AddAlreadyRaffledVariableRetry < ActiveRecord::Migration[8.0]
-  def change
-  end
-end
